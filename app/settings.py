be74e94e70a06c2028d87d3bb4187465bd67a3f3
Duke Nguyen <nguyentranduc1@gmail.com>
2017-03-02 12:35:10 -0800

    Test add 2d array

--- app/settings.py
+++ app/settings.py
@@ -4,7 +4,7 @@ Map = None
 
 def initializeMap(width, height):
 	global Map
-	Map = [[y for y in range(height)] for x in range(width)]
+	Map = [[0 for y in range(height)] for x in range(width)]
 
 def getMap(): # not sure if should approach Python with this OOP
 	global Map
