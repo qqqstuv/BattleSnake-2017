d6755e7646b97563fb147808bdcfb2b76ac56766
Duke Nguyen <nguyentranduc1@gmail.com>
2017-03-02 15:00:21 -0800

    Attemp 1 to make the snake not die

--- app/helper.py
+++ app/helper.py
@@ -14,9 +14,10 @@ def possibleMove(id, snakeCoords):
 			settings.getMap()[xy[0]][xy[1]] = 1
 		if snake.get('id') == id:
 			head = snake.get('coords')[0]
-	print settings.getMap()
-	print head
+	# print settings.getMap()
+	# print head
 	possibleAround(head, directions)
+	print directions
 	return directions
 
 #check four possible moves of the head
