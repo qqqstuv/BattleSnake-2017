78fa01bd4b475ae45f801a190e75c987473a4289
Duke Nguyen <nguyentranduc1@gmail.com>
2017-03-02 13:40:01 -0800

    Test add 2d array

--- app/helper.py
+++ app/helper.py
@@ -3,12 +3,13 @@ import settings
 
 # return all possible moves in one block surrounding area
 def possibleMove(coord, snakeCoords):
-	print settings.getMap()
+	debug(settings.getMap())
 	for snake in snakeCoords:
 		coordinates = snake.get('coords')
 		for xy in coordinates:
 			settings.getMap()[xy[0]][xy[1]] = 1
-	print settings.getMap()
+	debug(settings.getMap())
 
-def debug():
-	pass
+def debug(name):
+	print "Print in helper"
+	print name
