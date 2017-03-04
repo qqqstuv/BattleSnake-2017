5f3dcbe35e66734eee8bf2a2ae4b6b7ead829d5e
Duke Nguyen <nguyentranduc1@gmail.com>
2017-03-04 15:44:00 -0800

    delete class

--- app/dijkstra.py
+++ app/dijkstra.py
@@ -1,13 +1,5 @@
 import heapq
 
-class GridWithWeights(SquareGrid):
-    def __init__(self, width, height):
-        super().__init__(width, height)
-        self.weights = {}
-    
-    def cost(self, from_node, to_node):
-        return self.weights.get(to_node, 1)
-
 class PriorityQueue:
     def __init__(self):
         self.elements = []
