cb35b7c6c693a0f8a5684883e321782a67e8a7dd
Cameron Elwood <Celwood93@gmail.com>
2017-03-04 15:15:34 -0800

    adding stuff

--- app/HeatMap/heatMap.py
+++ app/HeatMap/heatMap.py
@@ -4,59 +4,141 @@ class heatMap:
     def __init__(self):
             pass
 
-    def findHeatMap(self, head, walls, num, width, height):
-        x_Size = assignStartEnd(head, num, width)
-        y_Size = assignStartEnd(head, num, height)
-        heatMap = {}
-        threatDepthConstant = 3
+    def findHeatMap(self, head, headList, walls, width, height):
+		num = 15 #has to be odd
+		threatDepthConstant = 3 #has to be odd divided by 2
+		borderWeight = 1 #borderWeight
+		beta = 1 # standard coefficient for border
+		theta = 1.5 # coefficient for duration
+		defaultHeadWeight = theta*100 #snakeHeadWeight multiplier
+		alpha = None
+		for i in headList:
+			walls.remove(i)
+			walls.append((i[0],defaultHeadWeight))
+		walls = addBorders(walls, width, height, borderWeight) #adds the borders to the list of walls
+		x_Size = assignStartEnd(head, num, width)
+		y_Size = assignStartEnd(head, num, height)
+		heatMap = {}
+		for wall in walls:
+			points = pointsAroundCoord(wall, threatDepthConstant, x_Size, y_Size) #returns a list of points of the form ((x,y),weight)
+			for point in points:
+				if heatMap.has_key(point):
+					heatMap[point] = heatMap[point] + points[point]
+				else:
+					heatMap[point] = points[point]
+			
 
-        for wall in walls:
-            xCoff = -threatDepthConstant
-            yCoff = -threatDepthConstant
-            if wall[0][0] < x_Size[0] or wall[0][0] > x_Size[1] or wall[0][1] < y_Size[0] or wall[0][1] > y_Size[1]:
-                continue
-            for x in range(wall[0][0]-threatDepthConstant, wall[0][0] + threatDepthConstant):
-               if(not(x<x_Size[0] or x>x_Size[1])):
-                    yCoff = -threatDepthConstant
-                    xCoff = xCoff + 1
-                    for y in range(wall[0][1]-threatDepthConstant, wall[0][1] + threatDepthConstant):
-                        if(not(y<y_Size[0] or y>y_Size[1])):
-                            yCoff = yCoff + 1
-                            weight = heatMap.get((x,y), 0)
-                            distanceLoad = (xCoff**2+yCoff**2)**(1/2.0)
-                            if distanceLoad == 0:
-                                distanceLoad = 1
-                            heatMap[(x,y)] = weight + wall[1]/distanceLoad
-        #print(heatMap)
-
-        return heatMap
+		return heatMap
+	
+def pointsAroundCoord(wall, num, x_Size, y_Size):
+	points = {}
+	point = wall[0]
+	startLeft = point[0] - num/2
+	endLeft = point[0] + num/2
+	startTop = point[1] - num/2
+	endTop = point[1] + num/2
+	for i in range(startLeft, endLeft+1):
+		for j in range(startTop, endTop+1):
+			if(i< x_Size[0] or j< y_Size[0] or (i==point[0] and j == point[1])):
+				continue
+			if( i> x_Size[1] or j > y_Size[1]):
+				continue
+			if points.has_key((i,j)):
+				points[(i,j)] = points[(i,j)] + wall[1]
+			else:
+				points[(i,j)] = wall[1]
+	
+	return points
+	
+def addBorders(walls, width, height, borderWeight):
+	for i in range(-1, width+1): #x = -1 to max, y = max
+		walls.append(((i,-1),borderWeight))
+	for j in range(-1, width+1):  #x = -1 to max, y = -1
+		walls.append(((j,height),borderWeight))
+	for k in range(0, height): #y = 0 to height-1, since x got it, and x = -1
+		walls.append(((-1, k), borderWeight))
+	for l in range(0, height):
+		walls.append(((width,l), borderWeight)) #y = 0 to height-1, and x = width
+	
+	return walls
 
 def assignStartEnd(head, num, maxDistance):
-    end = head[0]+num/2
-    start = head[0]-num/2
-    full = False #if end and start have to be resized, it sets them to the maxiumums
-    if start < 0:#if start is negative
-        addon = abs(start) #shaves off the negative
-        start = 0 #sets it to the minimum
-        end = end + addon #adds to the end of x_end
-        full = True #flag
-    elif end > maxDistance: #if end is too big
-        addon = end - maxDistance #snags the extra bit
-        start = start + addon #adds to start
-        end = maxDistance #set to maximum
-        full = True #flag
-    elif full: #sets both to maximums
-        start = 0
-        end = maxDistance  
-    return (start, end)     
+	end = head[0]+num/2
+	start = head[0]-num/2
+	full = False #if end and start have to be resized, it sets them to the maximums
+	if start < 0:#if start is negative
+		addon = abs(start) #shaves off the negative
+		start = 0 #sets it to the minimum
+		end = end + addon #adds to the end of x_end
+		full = True #flag
+	if end > maxDistance: #if end is too big
+		addon = end - maxDistance #snags the extra bit
+		start = start + addon #adds to start
+		end = maxDistance #set to maximum
+	else:
+		full = False
+		
+	if full: #sets both to maximums
+		start = 0
+		end = maxDistance
+	return (start, end)
 
-
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
                 
 def main():
-    A = heatMap()
-    j = [((4,4),16)]
-    k = (4,4)
-    print(A.findHeatMap(k,j,3, 10, 10))
+	A = heatMap()
+	j = [((5,5),6),((9,9),6),((1,1),6),((1,9), 6),((9,1),6)]
+	headSnakes = [((9,9),6), ((1,1),6)]
+	testB = []
+	testB = addBorders(testB, 3,3,5)
+	if len(testB) == 16:
+		print("border good")
+	else:
+		print("error")
+		print(testB)
+	wall = ((1,4),5)
+	num = 3
+	x_Size = (0,6)
+	y_Size = (0,6)
+	testA = pointsAroundCoord(wall, num, x_Size, y_Size)
+	for i in range(x_Size[1]):
+		print('\n')
+		for k in range(y_Size[1]):
+			if testA.has_key((i,k)):
+				print(int(testA[(i,k)]) ),
+			else:
+				print('-'),
+	
+	
+	k = (5,5)
+	x = 11
+	y = 11
+	for i in range(x):
+		print('\n')
+		for k in range(y):
+			passable = True
+			for p in range(len(j)):
+				if j[p][0] == (i,k):
+					print(j[p][1]),
+					passable = False
+			if passable:
+				print('-'),
+	print('\n\nMap')
+	k = (5,5)
+	x = 11
+	y = 11
+	map = A.findHeatMap(k,headSnakes, j, x, y)
+	for i in range(x):
+		print('\n')
+		for k in range(y):
+			if map.has_key((i,k)):
+				print(int(map[(i,k)]) ),
+			else:
+				print('-'),
+		
     
 
 
