2253d2f575a0a82387b57d2fb261699ede4681b9
Duke Nguyen <nguyentranduc1@gmail.com>
2017-03-04 07:20:54 -0800

    Change lookout constants

--- app/a_star.py
+++ app/a_star.py
@@ -90,8 +90,8 @@ def bfsGetWeight(graph, start):
 
 # walls is ((x,y), weight)
 def findHeatMap(head, walls, width, height):
-    num = 15
-    threatDepthConstant = 7
+    num = 17
+    threatDepthConstant = 5
     x_Size = assignStartEnd(head, num, width)
     y_Size = assignStartEnd(head, num, height)
     heatMap = {}
